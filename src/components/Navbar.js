e079bfd41ab9bc004f9fd6999ca1dc9033a02902
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-09-05 21:51:24 -0300

    Add reac-router with pages routes structure and a Navbar with links

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {NavLink} from "react-router-dom"
+import {ReactComponent as OfferIcon} from "../assets/svg/localOfferIcon.svg"
+import {ReactComponent as ExploreIcon} from "../assets/svg/exploreIcon.svg"
+import {ReactComponent as PersonOutlineIcon} from "../assets/svg/personOutlineIcon.svg"
+
+
+function Navbar() {
+  return (
+    <footer className='navbar'>
+        <nav className='navbarNav'>
+            <ul className='navbarListItems'>
+
+                <NavLink className='navbarListItem' to="/">
+                    <ExploreIcon width="36px" height="36px" />
+                    <p>Explore</p>
+                </NavLink>
+
+                <NavLink className='navbarListItem' to="/offers">
+                    <OfferIcon width="36px" height="36px" />
+                    <p>Offer</p>
+                </NavLink>
+
+                <NavLink className='navbarListItem' to="/profile">
+                    <PersonOutlineIcon width="36px" height="36px" />
+                    <p>Profile</p>
+                </NavLink>
+
+            </ul>
+        </nav>
+    </footer>
+  )
+}
+
+export default Navbar
