204637104209f256cc7d9fa75a6fcb09ef12cf6d
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-09-28 16:08:29 -0300

    Add 'CreateListing' Links and import

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Category from  "./pages/Category"
+import CreateListing from "./pages/CreateListing";
 
 
 
@@ -26,12 +27,13 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/category/:categoryName" element={<Category />} />
-          <Route path="profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
