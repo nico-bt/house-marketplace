a45bdc1515cb99e87d19d7a99c303c7557b6e049
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-09-29 21:47:52 -0300

    Add landlord Contact

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -7,14 +7,17 @@ import { db } from '../firebase.config'
 // Spinner and icon
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+import copyIcon from '../assets/svg/copyToClipboard.svg'
 import { toast } from 'react-toastify'
 
 function Listing() {
 
     const {categoryName, listingId} = useParams()
     const [listing, setListing] = useState(null)
+    const [landlord, setLandlord] = useState(null)
     const [isLoading, setisLoading] = useState(true)
     const [shareLinkCopied, setShareLinkCopied] = useState(false)
+    const [emailCopied, setEmailCopied] = useState(false)
 
     const navigate = useNavigate()
     const auth = getAuth()
@@ -45,7 +48,29 @@ function Listing() {
     // Options to format numbers as currency
     const options = { style: 'currency', currency: 'USD', maximumFractionDigits: 0, minimumFractionDigits: 0 }
     const numberFormat = new Intl.NumberFormat('en-US', options);
-      
+    
+    // Get Landlord data (user creator of the listing)
+    useEffect(() => {
+        const getLandlord = async () => {
+            try {
+                // Get doc Ref
+                const docRef = doc(db, 'users', listing.userRef)
+                const docSnap = await getDoc(docRef)
+          
+                if (docSnap.exists()) {
+                  setLandlord(docSnap.data())
+                } else {
+                  toast.error('Could not get landlord data')
+                }
+            } catch (error) {
+                console.log(error);
+                toast.error('Could not get landlord data')                
+            }
+          }
+          // After listing is filled with userRef, run getLandlord
+          listing && getLandlord()
+    }, [listing])
+    
 
     if(isLoading){
         return <Spinner />
@@ -99,10 +124,31 @@ function Listing() {
                     <li>{listing.furnished && 'Furnished'}</li>
                 </ul>
             </div>
-                
+
+            {landlord?.email && (
+                <>
+                <div className='contactLandlord-Container'>
+                    <a href={`mailto:${landlord.email}?Subject=${listing.name}`} className='contactLandlord' target="_blank">
+                        <h2>Contact Landlord:</h2>
+                        <p >Email: {landlord?.email}</p>
+                    </a>
+                    <img 
+                        src={copyIcon} 
+                        alt='copy email' 
+                        className='socialIconDiv'
+                        onClick={() => {
+                            navigator.clipboard.writeText(landlord.email)
+                            setEmailCopied(true)
+                            // For display a message "copied email" for 2 seconds
+                            setTimeout(() => {setEmailCopied(false)}, 2000)
+                        }}
+                    />
+                </div>
+                {emailCopied && <p className='copiedEmailMessage'>Email copied to clipboard!</p>}
+                </>
+            )}    
 
         </main>
   )
 }
-
 export default Listing
