d11100753bab0f67c90d4b682de7a60ab7188ac5
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-10-25 02:02:27 -0300

    Change to 'explore' to 'Home'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
 import {ReactComponent as OfferIcon} from "../assets/svg/localOfferIcon.svg"
-import {ReactComponent as ExploreIcon} from "../assets/svg/exploreIcon.svg"
+import {ReactComponent as HomeIcon} from "../assets/svg/homeIcon.svg"
 import {ReactComponent as PersonOutlineIcon} from "../assets/svg/personOutlineIcon.svg"
 
 
@@ -12,18 +12,18 @@ function Navbar() {
             <ul className='navbarListItems'>
 
                 <NavLink className='navbarListItem' to="/">
-                    <ExploreIcon width="36px" height="36px" />
-                    <p>Explore</p>
+                    <HomeIcon width="36px" height="36px" />
+                    <p className='navbarListItemName'>Home</p>
                 </NavLink>
 
                 <NavLink className='navbarListItem' to="/offers">
                     <OfferIcon width="36px" height="36px" />
-                    <p>Offers</p>
+                    <p className='navbarListItemName'>Offers</p>
                 </NavLink>
 
                 <NavLink className='navbarListItem' to="/profile">
                     <PersonOutlineIcon width="36px" height="36px" />
-                    <p>Profile</p>
+                    <p className='navbarListItemName'>Profile</p>
                 </NavLink>
 
             </ul>
